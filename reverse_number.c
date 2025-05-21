cc667314329a896eaf9b3c2e1e99025428c5f1c3
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-21 19:07:26 +0100

    modification du fichier reverse_number.c

--- reverse_number.c
+++ reverse_number.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-// Ce programme permet d'inverser les chiffres d'un entier
+
 int main() {
-int n, reversed = 0;
-scanf("%d", &n);
-while (n != 0) {
-reversed = reversed * 10 + n % n;
-n /= 10;
-}
-printf("Nombre inversé : %d\n", reversed);
-return 0;
+    int n, reversed = 0;
+    scanf("%d", &n);
+
+    while (n != 0) {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+
+    printf("Nombre inversé : %d\n", reversed);
+    return 0;
 }
