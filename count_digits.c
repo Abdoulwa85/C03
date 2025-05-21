db0944c01b691719cfb62225d3ee0baa10d9c629
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-21 19:10:53 +0100

    modification du fichier count_digits.c

--- count_digits.c
+++ count_digits.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-// Ce programme permet de compter combien de chiffres contient un entier
+
 int main() {
-int n, count = 0;
-scanf("%d", &n);
-if (n == 0)
-count = 1;
-else {
-while (n != 0) {
-n /= 10;
-count++;
-}
-}
-printf("%d contient %d chiffre(s).\n", n, count);
-return 0;
+    int n, count = 0;
+    scanf("%d", &n);
+
+    if (n == 0)
+        count = 1;
+    else {
+        while (n != 0) {
+            n /= 10;
+            count++;
+        }
+    }
+
+    printf("%d contient %d chiffre(s).\n", n, count);
+    return 0;
 }
