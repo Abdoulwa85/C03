d49e78e2866127401dc4489e39be0c88aea77532
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-21 19:14:45 +0100

    ajout du fichier fibonacci_series.c

---
+++ fibonacci_series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int limit, a = 0, b = 1, next;
+    scanf("%d", &limit);
+
+    while (a <= limit) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    printf("\n");
+    return 0;
+}
